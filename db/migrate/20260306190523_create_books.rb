6eb74a5c68e8d52f65652f159b4ed88586f752fe
Alejandro Hernández <alejandroohr@gmail.com>
2026-03-06 13:25:26 -0600

    Create models

---
+++ db/migrate/20260306190523_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[7.2]
+  def change
+    create_table :books do |t|
+      t.string :title, null: false
+      t.string :isbn, null: false
+      t.integer :total_copies, null: false, default: 1
+      t.references :genre, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

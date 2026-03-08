24b77a8e5274a7077adb8b3c247c5ce321ae1e68
Alejandro Hernández <alejandroohr@gmail.com>
2026-03-07 20:32:26 -0600

    Add borrowing policy specs

--- spec/policies/borrowing_policy_spec.rb
+++ spec/policies/borrowing_policy_spec.rb
@@ -1,27 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe BorrowingPolicy, type: :policy do
-  let(:user) { User.new }
+  let(:user) { create(:user, role: role) }
 
   subject { described_class }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
   permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    context 'when user is librarian' do
+      let(:role) { :librarian }
+
+      it 'allows access' do
+        expect(subject).to permit(user, Borrowing)
+      end
+    end
+    
+    context 'when user is member' do
+      let(:role) { :member }
+
+      it 'allows access' do
+        expect(subject).to permit(user, Borrowing)
+      end
+    end
   end
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  permissions :return_book? do
+    context 'when user is librarian' do
+      let(:role) { :librarian }
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+      it 'allows access' do
+        expect(subject).to permit(user, Borrowing)
+      end
+    end
+    
+    context 'when user is member' do
+      let(:role) { :member }
+
+      it 'allows access' do
+        expect(subject).to permit(user, Borrowing)
+      end
+    end
   end
 end
